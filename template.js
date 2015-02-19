312a9b4cedfae3720fce8288470c7cf3438130d3
miya0001 <miya@wpist.me>
2015-02-19 14:37:15 +0900

    first commit

---
+++ template.js
@@ -0,0 +1,34 @@
+/**
+ * iemoto
+ * https://github.com/megumiteam/iemoto/blob/master/template.js
+ *
+ * Licensed under the GPLv2
+ */
+
+'use strict';
+
+exports.description = 'Create a WordPress theme based on Automattic\'s _s starter theme.';
+
+exports.notes = '';
+exports.after = '';
+exports.warnOn = '*';
+
+// The actual init template
+exports.template = function( grunt, init, done ) {
+    init.process( {}, [
+        // Prompt for these values.
+        init.prompt( 'hostname', 'vccw.dev' ),
+        init.prompt( 'ip', '192.168.55.10' ),
+        init.prompt( 'lang', 'en_US' ),
+        {
+            name: 'gulp',
+            message: 'Use gulp?',
+            default: 'y/N'
+        }
+    ], function( err, props ) {
+        props.keywords = [];
+        props.version = '0.1.0';
+        // Done!
+        done();
+    });
+};
