5d3a8b0520571c37bae4a9c9a7ae529415076fa4
miya0001 <miya@wpist.me>
2015-02-20 02:30:38 +0900

    generating site.yml

--- template.js
+++ template.js
@@ -16,9 +16,13 @@ exports.warnOn = '*';
 // The actual init template
 exports.template = function( grunt, init, done ) {
     init.process( {}, [
-        // nothing
+        init.prompt( 'hostname', 'vccw.dev' ),
+        init.prompt( 'ip', '192.168.55.10' ),
+        init.prompt( 'wp_lang', 'en_US' ),
+        init.prompt( 'wp_version', 'latest' )
     ], function( err, props ) {
         var files = init.filesToCopy(props);
+        files['site.yml'] = 'vccw/templates/site.yml'
         init.copyAndProcess(files, props);
         done();
     });
